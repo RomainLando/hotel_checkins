5f463dde0b5d8ae5173a3fbfda947a512ba809d3
Romain Landolfini <romain.landolfini@gmail.com>
2022-12-14 16:16:43 +0000

    :pencil2: fixed typo on seeds.js

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,15 +3,15 @@ use hotel_checkins
 db.dropDatabase()
 db.guests.insertMany([
     {"name":"Frodo Baggins",
-    emailAdress:"f.baggins@yahoo.shire",
+    "emailAddress":"f.baggins@yahoo.shire",
     checkedInStatus:false},
     {"name":"Elrond",
-    emailAdress:"lord.of.Rivendell@gmail.com",
+    "emailAddress":"lord.of.Rivendell@gmail.com",
     checkedInStatus:false},
     {"name":"Samwise Gamgee",
-    emailAdress:"s.gamgee@landscape.gardening.shire",
+    "emailAddress":"s.gamgee@landscape.gardening.shire",
     checkedInStatus:false},
     {"name":"Strider",
-    emailAdress:"Aragorn@prancing.poney.com",
+    "emailAddress":"Aragorn@prancing.poney.com",
     checkedInStatus:true}
 ])
